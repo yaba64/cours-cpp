288f76127bc745f51378b545510795f12c719948
Marc Salesses <salesses.marc@gmail.com>
2018-10-23 18:54:10 +0200

    Ajout des derniers exo

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-#include<math.h>
-using namespace std ;
-
-
-main()
-{
-
+using namespace std;
+//: C03:PointerArithmetic.cpp
+#define P(EX) cout << #EX << ": " << EX << endl;
+int main() {
+int a[10];
+for(int i = 0; i < 10; i++)
+a[i] = i; // Attribue les valeurs de l?index
+int* ip = a;
+P(*ip);
+P(*++ip);
+P(*(ip + 5));
+int* ip2 = ip + 5;
+P(*ip2);
+P(*(ip2 - 4));
+P(*--ip2);
+P(ip2 - ip); // Renvoie le nombre d?éléments
 }
--- test2/main.cpp
+++ test2/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
+//: C03:PassAddress.cpp
+void f(int &p)
+{
+    cout << "p = " << p << endl;
 
+    p = 5;
+    cout << "p = " << p << endl;
+}
 int main()
 {
-
-
-    cout << "L'adresse de 'ageUtilisateur' est : " << endl; // case memoire de la variable
-
-
-
-    return 0;
+    int x = 47;
+    cout << "x = " << x << endl;
+    cout << "&x = " << &x << endl;
+    f(x);
+    cout << "x = " << x << endl;
 }
